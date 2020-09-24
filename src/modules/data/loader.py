91ac7c46bbbe3febd241eed469c01031196dbda7
Thomas Bailly <3940727+Authfix@users.noreply.github.com>
2020-09-24 13:25:40 +0200

    feat(split): break raw data into multiple files
    fix(split): write file header back

--- src/modules/data/loader.py
+++ src/modules/data/loader.py
@@ -1,14 +1,45 @@
-from os import path, listdir
+from os import path, listdir, getcwd
 
+import csv
 import requests
 import sys
 import zipfile
 
+import pandas as pd
+
 ZIP_REMOTE_PATH = 'https://stdatalake005.blob.core.windows.net/public/movies_dataset.zip'
 ZIP_LOCAL_PATH = '../data/movies_dataset.zip'
 RAW_LOCAL_PATH = '../data/RAW/'
+CURATED_LOCAL_PATH = '../data/CURATED/'
+
+TITLE_FILE_NAME = 'title.basics.tsv'
 
 class IntermovieDataLoader:
+
+    def split_data(self):
+        '''
+        Break raw data into many files
+        '''
+        
+        with open(RAW_LOCAL_PATH + TITLE_FILE_NAME, encoding='utf-8') as file_stream:    
+            file_stream_reader = csv.DictReader(file_stream, delimiter='\t')
+            
+            open_files_references = {}
+
+            for row in file_stream_reader:
+                title_type = row['titleType']
+
+                # Open a new file and write the header
+                if title_type not in open_files_references:
+                    output_file = open(CURATED_LOCAL_PATH + '{}.csv'.format(title_type), 'w', encoding='utf-8', newline='')
+                    dictionary_writer = csv.DictWriter(output_file, fieldnames=file_stream_reader.fieldnames)
+                    dictionary_writer.writeheader()
+                    open_files_references[title_type] = output_file, dictionary_writer
+                # Always write the row
+                open_files_references[title_type][1].writerow(row)
+            # Close all the files
+            for output_file, _ in open_files_references.values():
+                output_file.close()
 
     def ensure_data_loaded(self):
         '''
